c5b82ccd6618d8b4c20d7fdb20e78baf0b59d359
Seiha <naingseiha@gmail.com>
2025-04-16 10:08:06 +0200

    Update project

--- main.rkt
+++ main.rkt
@@ -1,23 +1,46 @@
 #lang racket
 
-(require web-server/servlet
-         web-server/servlet-env
-         "pages/home.rkt"
-         "pages/info.rkt"
-         "pages/contact.rkt"
-         "pages/about.rkt")
+(require web-server/http
+         web-server/http/xexpr
+         web-server/http/bindings
+         web-server/servlet-env)
 
-(define-values (start dispatcher)
-  (dispatch-rules
-   [("home") home-page]
-   [("info") info-page]
-   [("contact") contact-page]
-   [("about") about-page]
-   [else home-page]))
+;; Define xexpr-response manually (fix for Racket 8.15)
+(define (xexpr-response x)
+  (response/xexpr x))
 
-(serve/servlet dispatcher
-               #:launch-browser? #t
-               #:servlet-path "/"
+(define (home-page req)
+  ;; Extract form data properly using request-bindings
+  (define bindings
+    (request-bindings/raw req))
+  
+  (define input-text 
+    (if (exists-binding? 'input-text bindings)
+        (extract-binding/single 'input-text bindings)
+        #f))
+  
+  ;; Create the base content
+  (define content
+    `(html
+      (head (title "Home - Racket Web App"))
+      (body
+       (div ((class "content"))
+            (h1 "Welcome to Racket Web App")
+            (p "Racket is a general-purpose, multi-paradigm programming language based on Scheme.")
+            (h2 "Enter something below:")
+            ;; Form for user input
+            (form ((method "POST"))
+                  (label "Enter some text: ")
+                  (input ((type "text") (name "input-text")))
+                  (button ((type "submit")) "Submit"))
+            ,@(if input-text
+                  `((p ,(string-append "You entered: " input-text)))
+                  '())))))
+  
+  (xexpr-response content))
+
+;; Start the server
+(serve/servlet home-page
                #:port 8080
-               #:servlet-regexp #rx""
-               #:extra-files-paths (list "static"))
+               #:servlet-path "/"
+               #:launch-browser? #t)
